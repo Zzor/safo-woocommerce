89f2c225354af017075a05815eef3a7ad6afe567
Zzor <hugo.salazar.cardozo@gmail.com>
2022-03-08 15:22:42 +0100

    feat(topbar): mise en position fixed

--- wp-content/themes/cyrille-mulot/front-page.php
+++ wp-content/themes/cyrille-mulot/front-page.php
@@ -1,7 +1,15 @@
 <?php get_header(); ?>
 
-<h1>
-  Accueil
-</h1>
+<main class="p-home">
+  <header class="p-home__hero">
+    <picture class="p-home__hero-picture">
+      <?php $home_hero_image = get_field('home_hero_image'); ?>
+      <?php if($home_hero_image) : ?>
+        <img src="<?= esc_url($home_hero_image['url']); ?>" 
+            alt="<?= esc_attr($home_hero_image['alt']); ?>" />
+      <?php endif; ?>
+    </pictu>
+  </header>
+</main>
 
 <?php get_footer(); ?>
