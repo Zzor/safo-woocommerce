293aecf49f0ab7e19836fbfa139011173e503d9f
Zzor <hugo.salazar.cardozo@gmail.com>
2022-03-08 14:27:48 +0100

    feat(buttons): integration des c-button-square

--- wp-content/themes/cyrille-mulot/footer.php
+++ wp-content/themes/cyrille-mulot/footer.php
@@ -7,14 +7,14 @@
       </div>
 
       <ul class="l-footer__socials">
-        <li class="l-footer__social-item">
+        <li class="c-button-square">
           <a href="https://facebook.com/<?= get_option('facebook'); ?>" aria-label="facebook" target="_blank">
             <span class="text-for-seo">Facebook</span>
             <?php require get_template_directory() . '/template-parts/social-icons/icon-facebook.php'; ?>
           </a>
         </li>
 
-        <li class="l-footer__social-item">
+        <li class="c-button-square">
           <a href="https://instagram.com/<?= get_option('instagram'); ?>" aria-label="instagram" target="_blank">
             <span class="text-for-seo">instagram</span>
             <?php require get_template_directory() . '/template-parts/social-icons/icon-instagram.php'; ?>
