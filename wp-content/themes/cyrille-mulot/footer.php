ee5be4e31f06e106d45b66a19f73b83eb6e7a39e
Zzor <hugo.salazar.cardozo@gmail.com>
2022-03-08 14:13:28 +0100

    feat(footer): integration full responsive

--- wp-content/themes/cyrille-mulot/footer.php
+++ wp-content/themes/cyrille-mulot/footer.php
@@ -1,3 +1,38 @@
+  <footer class="l-footer">
+    <div class="l-footer__inner">
+      <div class="l-footer__follow">
+        <a href="https://www.instagram.com/cyrillemulot_photography/" target="_blank">
+          Suivez-moi sur instagram
+        </a>
+      </div>
+
+      <ul class="l-footer__socials">
+        <li class="l-footer__social-item">
+          <a href="https://facebook.com/<?= get_option('facebook'); ?>" aria-label="facebook" target="_blank">
+            <span class="text-for-seo">Facebook</span>
+            <?php require get_template_directory() . '/template-parts/social-icons/icon-facebook.php'; ?>
+          </a>
+        </li>
+
+        <li class="l-footer__social-item">
+          <a href="https://instagram.com/<?= get_option('instagram'); ?>" aria-label="instagram" target="_blank">
+            <span class="text-for-seo">instagram</span>
+            <?php require get_template_directory() . '/template-parts/social-icons/icon-instagram.php'; ?>
+          </a>
+        </li>
+      </ul>
+
+      <div class="l-footer__infos">
+        <p class="l-footer__copyright">
+          2022 - Cyrille Mulot - Tous droits réservés
+        </p>
+        <p class="l-footer__credits">
+          Développé par <a href="https://hugo-salazar.com" target="_blank">Hugo Salazar</a>
+        </p>
+      </div>
+    </div>
+  </footer>
+  
   </div> <!-- Close for-mobile-hidden -->
   <?php wp_footer(); ?>
 </body>
---
+++ wp-content/themes/cyrille-mulot/template-parts/social-icons/icon-facebook.php
@@ -0,0 +1,11 @@
+<svg aria-hidden="true" 
+     role="img" 
+     focusable="false" 
+     xmlns="http://www.w3.org/2000/svg" 
+     width="16" 
+     height="16" 
+     viewBox="0 0 7 12" 
+     fill="currentColor" 
+     xmlns="https://www.w3.org/2000/svg">
+  <path d="M5.27972 1.99219H6.30215V0.084375C6.12609 0.0585937 5.51942 0 4.81306 0C3.33882 0 2.32912 0.99375 2.32912 2.81953V4.5H0.702148V6.63281H2.32912V12H4.32306V6.63281H5.88427L6.13245 4.5H4.32306V3.03047C4.32306 2.41406 4.47791 1.99219 5.27972 1.99219Z"></path>
+</svg>
