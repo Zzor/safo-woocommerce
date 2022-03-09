85b676738556ceec67387e64c4de2e044145abf6
Zzor <hugo.salazar.cardozo@gmail.com>
2022-03-09 23:54:23 +0100

    feat(home-categories): integration desktop + ACF

--- wp-content/themes/cyrille-mulot/front-page.php
+++ wp-content/themes/cyrille-mulot/front-page.php
@@ -50,11 +50,6 @@
         <h2>
           <?php the_field('home_categories_title'); ?>  
         </h2>
-
-        <p>
-          We are fine-art, wedding & portrait film photographers from Oregon, with a special love for natural light,
-          medium format film cameras & redheads with freckles. With over 5 years of experience, numerous workshops and features in top wedding publications, I capture beauty in the most subtle. 
-        </p>
       </header>
 
       <ul>
@@ -100,6 +95,26 @@
 
         <?php if(have_rows('home_categories_photo_3')) : ?>
           <?php while(have_rows('home_categories_photo_3')) : the_row(); ?>
+            <li>
+              <a href="<?= get_sub_field('link'); ?>">
+                  <?php $photo = get_sub_field('photo'); ?>
+                  <?php if($photo) : ?>
+                    <figure>
+                      <img src="<?= esc_url($photo['url']); ?>" 
+                          alt="<?= esc_attr($photo['alt']); ?>" />
+
+                      <figcaption>
+                        <?php the_sub_field('title'); ?>
+                      </figcaption>
+                    </figure>
+                  <?php endif; ?>
+              </a>
+            </li>
+          <?php endwhile; ?>
+        <?php endif; ?>
+
+        <?php if(have_rows('home_categories_photo_4')) : ?>
+          <?php while(have_rows('home_categories_photo_4')) : the_row(); ?>
             <li>
               <a href="<?= get_sub_field('link'); ?>">
                   <?php $photo = get_sub_field('photo'); ?>
