17611f4da85097b695c2a37f2e50484af6f5681d
Zzor <hugo.salazar.cardozo@gmail.com>
2022-03-10 01:03:56 +0100

    feat(grid): mise à jour

--- wp-content/themes/cyrille-mulot/front-page.php
+++ wp-content/themes/cyrille-mulot/front-page.php
@@ -2,30 +2,32 @@
 
 <main class="p-home">
   <header class="p-home__hero">
-    <picture class="p-home__hero-picture">
-      <?php $home_hero_image = get_field('home_hero_image'); ?>
-      <?php if($home_hero_image) : ?>
-        <img src="<?= esc_url($home_hero_image['url']); ?>" 
-            alt="<?= esc_attr($home_hero_image['alt']); ?>" 
-            class="p-home__hero-img" />
-      <?php endif; ?>
+    <div class="p-home__hero-inner">
+      <picture class="p-home__hero-picture">
+        <?php $home_hero_image = get_field('home_hero_image'); ?>
+        <?php if($home_hero_image) : ?>
+          <img src="<?= esc_url($home_hero_image['url']); ?>" 
+              alt="<?= esc_attr($home_hero_image['alt']); ?>" 
+              class="p-home__hero-img" />
+        <?php endif; ?>
 
-      <div class="p-home__hero-picture-shade"></div>
-    </picture>
+        <div class="p-home__hero-picture-shade"></div>
+      </picture>
 
-    <div class="p-home__hero-title-wrapper">
-      <h1 class="p-home__hero-title">
-        <?php the_field('home_hero_title'); ?>
-      </h1>
+      <div class="p-home__hero-title-wrapper">
+        <h1 class="p-home__hero-title">
+          <?php the_field('home_hero_title'); ?>
+        </h1>
 
-      <p class="p-home__hero-subtitle">
-        <?php the_field('home_hero_subtitle'); ?>
-      </p>
+        <p class="p-home__hero-subtitle">
+          <?php the_field('home_hero_subtitle'); ?>
+        </p>
+      </div>
+
+      <a href="#link-home-description" class="c-button-square is-colored p-home__hero-scroll-down">
+        <img src="<?= THEME_IMG ?>/arrow-down.svg" alt="">
+      </a>
     </div>
-
-    <a href="#link-home-description" class="c-button-square is-colored p-home__hero-scroll-down">
-      <img src="<?= THEME_IMG ?>/arrow-down.svg" alt="">
-    </a>
   </header>
 
   <section id="link-home-description" class="p-home__presentation">
