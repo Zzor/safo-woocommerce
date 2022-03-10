4f23ea5fedb0ba93bf4d7d0009e760edc6e5afc9
Zzor <hugo.salazar.cardozo@gmail.com>
2022-03-10 13:07:53 +0100

    feat(basic-template): creation du template basique pour les pages d'informations type CGU, conditions de ventes, etc

---
+++ wp-content/themes/cyrille-mulot/templates/basic.php
@@ -0,0 +1,21 @@
+<?php 
+/**
+ * Template Name: Basique
+ */
+get_header();
+?>
+
+<section class="p-basic-template">
+  <div class="p-basic-template__inner">
+    <h1 class="p-basic-template__title">
+      <?php the_title(); ?>
+    </h1>
+
+    <div class="p-basic-template__wysiwyg">
+      <?= get_the_content(); ?>
+    </div>
+  </div>
+</section>
+
+
+<?php get_footer(); ?>
