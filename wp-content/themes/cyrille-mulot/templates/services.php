dcb40039b4bb2d91f6cd43bc5c8028952d2a2f9f
Zzor <hugo.salazar.cardozo@gmail.com>
2022-03-14 13:11:01 +0100

    fet(services): design de base

--- wp-content/themes/cyrille-mulot/templates/services.php
+++ wp-content/themes/cyrille-mulot/templates/services.php
@@ -8,7 +8,47 @@
 
 <main class="t-services">
   <div class="t-services__inner">
-    Atta, j'ai pas encore commencé à la faire !
+    <ul>
+      <li>
+        <div class="text-wrap">
+          <div class="title">
+            Photos de voyage
+          </div>
+
+          <div class="subtitle">
+            Phasellus faucibus venenatis dolor.
+          </div>
+
+          <div class="text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, quidem. Eum officia quos vero alias ab temporibus fugiat minus! Repellendus, quo perferendis eaque facere doloremque nostrum similique quibusdam distinctio officia.
+          </div>
+        </div>
+
+        <div class="img-wrap">
+          <img src="https://wiso.foxthemes.me/wp-content/uploads/2018/04/cameron-stow-243141.jpg" alt="">
+        </div>
+      </li>
+
+      <li>
+        <div class="img-wrap">
+          <img src="https://wiso.foxthemes.me/wp-content/uploads/2018/04/grant-ritchie-513607.jpg" alt="">
+        </div>
+
+        <div class="text-wrap">
+          <div class="title">
+            Photos de voyage
+          </div>
+
+          <div class="subtitle">
+            Phasellus faucibus venenatis dolor.
+          </div>
+
+          <div class="text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, quidem. Eum officia quos vero alias ab temporibus fugiat minus! Repellendus, quo perferendis eaque facere doloremque nostrum similique quibusdam distinctio officia.
+          </div>
+        </div>
+      </li>
+    </ul>
   </div>
 </main>
 
