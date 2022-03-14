d6c6994f63781ca4ff9f80ca0a3b0f18a90f219f
Zzor <hugo.salazar.cardozo@gmail.com>
2022-03-14 12:45:45 +0100

    feat(gallerie): changement du template de gallerie en Services

--- wp-content/themes/cyrille-mulot/template-parts/topnav.php
+++ wp-content/themes/cyrille-mulot/template-parts/topnav.php
@@ -22,8 +22,7 @@
     <a class="cart-customlocation" 
        href="<?= wc_get_cart_url(); ?>" 
        title="<?php _e( 'Voir le panier' ); ?>">
-        <?= sprintf (_n('%d article', '%d articles', WC()->cart->get_cart_contents_count()), 
-            WC()->cart->get_cart_contents_count()); ?> – <?= WC()->cart->get_cart_total(); ?>
+        <?= sprintf (_n('%d article', '%d articles', WC()->cart->get_cart_contents_count()), WC()->cart->get_cart_contents_count()); ?> – <?= WC()->cart->get_cart_total(); ?>
     </a>
 
     <button id="js-burger" class="l-nav__burger" aria-label="menu">
