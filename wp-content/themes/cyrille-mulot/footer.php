ee575e8bd28d3a7725b23d5826c43bc7be74f5b9
Zzor <hugo.salazar.cardozo@gmail.com>
2022-03-17 00:21:56 +0100

    feat(footer): mise à jour du copyright

--- wp-content/themes/cyrille-mulot/footer.php
+++ wp-content/themes/cyrille-mulot/footer.php
@@ -74,7 +74,7 @@
 
       <div class="l-footer__bottom">
         <p class="l-footer__copyright">
-          <?= date('Y'); ?> - Cyrille Mulot - Tous droits réservés
+          <?= date('Y'); ?> - Safo - Tous droits réservés
         </p>
         <p class="l-footer__credits">
           Développé par <a href="https://hugo-salazar.com" target="_blank">Hugo Salazar</a>
