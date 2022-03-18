98ad2782ad9264c9b15914c93998523f91e02c80
Zzor <hugo.salazar.cardozo@gmail.com>
2022-03-18 12:20:58 +0100

    feat(topnav): gestion de l'animation open-close du burger

--- wp-content/themes/cyrille-mulot/assets/js/burger-menu.js
+++ wp-content/themes/cyrille-mulot/assets/js/burger-menu.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
   $('#js-burger').click(function() {
     $('.l-topnav__principal-nav').toggleClass('is-opened');
-    $('.l-nav__bottom').toggleClass('is-active');
-    $('.l-nav__burger span').toggleClass('is-active');
+    $('.l-topnav__burger span').toggleClass('is-active');
   });
 });
